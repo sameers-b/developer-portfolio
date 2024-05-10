1343ac0456b0f310dedcdfbbc76fd6fa122cd656
Sameer Singh <sameer@shadowcast.io>
2024-05-10 12:36:56 +0530

    new-ui

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -6,7 +6,7 @@ export const personalData = {
     "My name is Sameer Singh. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: "sameersingh7458@gmail.com",
   phone: "+917458855350",
-  address: "Delhi India",
+  address: "Noida India",
   github: "https://github.com/sameers-b",
   facebook: "",
   linkedIn: "https://www.linkedin.com/in/sameer-singh-a79178186/",
